f6004606ae3b94bfa78080ce7436f55eb5b33408
Soo Hyeon Kim <kimgooneya@gmail.com>
2025-06-22 23:55:35 +0900

    변경

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pdf_reader import PDFReader
 def main():
     print("Hello from lm-translator!")
     myllm = MyLocalLLM('qwen/qwen3-8b')
-    mypdfreader= PDFReader("C:\story\Sudden Curve\Sudden_Curve.pdf")
+    mypdfreader= PDFReader("filepath.pdf")
     print(mypdfreader.read_pages_with_pypdf2())
 
     
